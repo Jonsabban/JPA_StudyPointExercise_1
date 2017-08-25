b000c1fa1900e6738bcc636cbd822b40fff82563
Jonsabban <Sanox3@hotmail.dk>
2017-08-25 15:43:50 +0200

    Add files via upload

--- SP_JPA2/src/main/java/jpaControl/CRUD.java
+++ SP_JPA2/src/main/java/jpaControl/CRUD.java
@@ -7,6 +7,7 @@ package jpaControl;
 
 import entity.Book;
 import entity.EBook;
+import entity.PaperBook;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -90,6 +91,102 @@ public class CRUD {
         } finally {
             em.close();
         }
+    }
 
+    
+    public EBook findEBook(int isbn) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(EBook.class, isbn);
+        } finally {
+            em.close();
+        }
+    }
+
+    public EBook editEBook(EBook ebook) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            ebook = em.merge(ebook);
+            em.getTransaction().commit();
+            return ebook;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroyEBook(int isbn) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Book book = null;
+            book = em.getReference(Book.class, isbn);
+            book.getIsbn();
+            em.remove(book);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+
+        }
+    }
+    
+     public void createPBook(int isbn, int instock, double shippingweight) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            PaperBook pbook = new PaperBook();
+            pbook.setInStock(instock);
+            pbook.setShippingWeight(shippingweight);
+            Book b = findBook(isbn);
+            if (b != null) {
+                pbook.setIsbn(b.getIsbn());
+                em.persist(pbook);
+                em.getTransaction().commit();
+            }
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public Book findPBook(int isbn) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(PaperBook.class, isbn);
+        } finally {
+            em.close();
+        }
+    }
+
+    public Book editPBook(PaperBook book) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            book = em.merge(book);
+            em.getTransaction().commit();
+            return book;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroyPBook(int isbn) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Book book = null;
+            book = em.getReference(PaperBook.class, isbn);
+            book.getIsbn();
+            em.remove(book);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+
+        }
     }
 }
